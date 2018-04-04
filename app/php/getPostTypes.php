5f2b468f99ff766a1609221a224db5a9111c078f
Keir Forster <keirforster@gmail.com>
2018-04-04 03:45:36 -0700

    updated header content types for php scripts

--- app/php/getPostTypes.php
+++ app/php/getPostTypes.php
@@ -1,5 +1,5 @@
 <?php
-    header('Content-type: text/html; charset=utf-8');
+    header('Content-type: application/json; charset=utf-8');
     require 'db/DbUtil.php';
     require 'db/SqlQuery.php';
     
